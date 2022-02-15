ca009ab97fc0b02c7d769423e71b28a4d96db7a2
Nathan Page <33336363+ndpage@users.noreply.github.com>
2022-02-15 12:00:18 -0500

    Added views for poll detials, results, and vote

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,7 @@
 from django.http import HttpResponse
 
-
 def index(request):
     return HttpResponse("Hello, world. eb698350 is the polls index.")
-
 
 def hello(request):
     return HttpResponse("Hello, world!")
