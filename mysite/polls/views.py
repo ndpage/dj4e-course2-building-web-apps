f7665a777a6ff5c99c2b0daeecbaaf0efef434ee
Nathan Page <33336363+ndpage@users.noreply.github.com>
2022-02-23 14:50:04 -0500

    Replaced generic code with code to vote on a poll

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from django.shortcuts import get_object_or_404
-
-
-from .models import Question
+from .models import Choice, Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -22,8 +20,24 @@ def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
+# vote method sends POST request to server to vote on a particular poll
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 def owner(request):
     return HttpResponse("Hello, world. 3d2d3096 is the polls index.")
