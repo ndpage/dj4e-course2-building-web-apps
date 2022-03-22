44db8616faa9ce56bc2c373aa9b54cf6c750a2b0
Nathan Page <33336363+ndpage@users.noreply.github.com>
2022-03-21 23:02:42 -0400

    Changed reponse to show the number of visits count

--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -3,13 +3,17 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello(request):
-  cookie_key = 'dj4e_cookie'
-  cookie_value = '27ec7e21'
-
+  
+  num_visits = request.session.get('num_visits', 0) + 1
+  request.session['num_visits'] = num_visits 
+  if num_visits > 4 : del(request.session['num_visits'])
+  
   print(request.COOKIES)
   oldval = request.COOKIES.get('dj4e_cookie', None)
-  print(f"Previous session cookie: {oldval}")
-  
-  resp = HttpResponse(f'You set the cookie {cookie_key}:{cookie_value}')
+  print(f"Previous session cookie: {oldval}")  
+
+  cookie_key = 'dj4e_cookie'
+  cookie_value = '27ec7e21'
+  resp = HttpResponse(f'view count={str(num_visits)}')
   resp.set_cookie(cookie_key, cookie_value, max_age=1000)
   return resp
