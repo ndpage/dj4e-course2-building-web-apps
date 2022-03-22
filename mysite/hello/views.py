1ab87f0878584572b46457a0aac1239471deb221
Nathan Page <33336363+ndpage@users.noreply.github.com>
2022-03-21 22:48:23 -0400

    created variables for setting cookie key and value

--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -3,10 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello(request):
+  cookie_key = 'dj4e_cookie'
+  cookie_value = '27ec7e21'
+
   print(request.COOKIES)
   oldval = request.COOKIES.get('dj4e_cookie', None)
   print(f"Previous session cookie: {oldval}")
   
   resp = HttpResponse('You set the cookie!')
-  resp.set_cookie('dj4e_cookie', '27ec7e21', max_age=1000)
+  resp.set_cookie(cookie_key, cookie_value, max_age=1000)
   return resp
